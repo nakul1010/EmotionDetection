df873edd82891b12f4578ace2b19b366de9297fc
Nakul Amate <58786560+nakul1010@users.noreply.github.com>
2020-08-13 20:46:47 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -12,7 +12,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(-1)
+        self.video = cv2.VideoCapture(0)
 
     def __del__(self):
         self.video.release()
